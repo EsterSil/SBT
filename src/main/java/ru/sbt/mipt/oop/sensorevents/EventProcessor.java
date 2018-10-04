a1237248733b334d7ea0055bd0cb8b15be9cb2ea
estersil <lernichenko@phystech.edu>
2018-10-04 17:30:47 +0300

    Refactoring part 1, update

--- src/main/java/ru/sbt/mipt/oop/sensorevents/EventProcessor.java
+++ src/main/java/ru/sbt/mipt/oop/sensorevents/EventProcessor.java
@@ -1,8 +1,7 @@
 package ru.sbt.mipt.oop.sensorevents;
 
-import ru.sbt.mipt.oop.*;
+import javafx.util.Pair;
 import ru.sbt.mipt.oop.service.SensorService;
-import ru.sbt.mipt.oop.service.SensorCommand;
 import ru.sbt.mipt.oop.smarthomeobjects.Door;
 import ru.sbt.mipt.oop.smarthomeobjects.Light;
 import ru.sbt.mipt.oop.smarthomeobjects.Room;
@@ -13,51 +12,43 @@ import static ru.sbt.mipt.oop.sensorevents.SensorEventType.*;
 
 public class EventProcessor {
     public static void processDoorEvent(SmartHome smartHome, SensorEvent event, SensorService eventService) {
-        for (Room room : smartHome.getRooms()) {
-            for (Door door : room.getDoors()) {
-                if (door.getId().equals(event.getObjectId())) {
-                    if (event.getType() == DOOR_OPEN) {
-                        door.setOpen(true);
-                        System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
-                    } else {
-                        door.setOpen(false);
-                        System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
-                        // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
-                        // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
-                        if (room.getName().equals("hall")) {
-                            offLightTotal(smartHome, eventService);
-                        }
-                    }
-                }
-            }
-        }
-    }
+        Pair<Door, Room> involved = smartHome.getDoorRoomByID(event.getObjectId());
+        Door involvedDoor = involved.getKey();
+        Room involvedRoom = involved.getValue();
 
-    private static void offLightTotal(SmartHome smartHome, SensorService eventService) {
-        for (Room homeRoom : smartHome.getRooms()) {
-            for (Light light : homeRoom.getLights()) {
-                light.setOn(false);
-                SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
-                eventService.sendCommand(command);
+        if (involvedDoor == null || involvedRoom == null) {
+            return;
+        }
+
+        if (event.getType() == DOOR_OPEN) {
+            involvedDoor.setOpen(true);
+            System.out.println("Door " + involvedDoor.getId() + " in room " + involvedRoom.getName() + " was opened.");
+        } else {
+            involvedDoor.setOpen(false);
+            System.out.println("Door " + involvedDoor.getId() + " in room " + involvedRoom.getName() + " was closed.");
+            // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
+            // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
+            if (involvedRoom.getName().equals("hall")) {
+                smartHome.offLightTotal(eventService);
             }
         }
+
     }
 
 
     public static void processLightEvent(SmartHome smartHome, SensorEvent event) {
-        for (Room room : smartHome.getRooms()) {
-            for (Light light : room.getLights()) {
-                if (light.getId().equals(event.getObjectId())) {
-                    if (event.getType() == LIGHT_ON) {
-                        light.setOn(true);
-                        System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned on.");
-                    } else {
-                        light.setOn(false);
-                        System.out.println("Light " + light.getId() + " in room " + room.getName() + " was turned off.");
-                    }
-                }
-            }
+
+        Pair<Light, Room> involved = smartHome.getLightByID(event.getObjectId());
+        Light involvedLight = involved.getKey();
+        Room involvedRoom = involved.getValue();
+        if (event.getType() == LIGHT_ON) {
+            involvedLight.setOn(true);
+            System.out.println("Light " + involvedLight.getId() + " in room " + involvedRoom.getName() + " was turned on.");
+        } else {
+            involvedLight.setOn(false);
+            System.out.println("Light " + involvedLight.getId() + " in room " + involvedRoom.getName() + " was turned off.");
         }
     }
+
 
 }
