5ac81faf529fcdad99cfca27b193987986ab4d24
estersil <lernichenko@phystech.edu>
2018-11-19 17:45:28 +0300

    some tests  added

--- Edu/smart-home/src/main/java/ru/sbt/mipt/oop/remotecontrol/Controller.java
+++ Edu/smart-home/src/main/java/ru/sbt/mipt/oop/remotecontrol/Controller.java
@@ -27,44 +27,9 @@ public class Controller implements RemoteControl {
         }
     }
 
-    public Controller onButtonA(Command command) {
-        buttonMap.put("A", command);
+    public Controller setCommandOnButton(String buttonCode, Command command) {
+        buttonMap.put(buttonCode, command);
         return this;
     }
 
-    public Controller onButtonB(Command command) {
-        buttonMap.put("B", command);
-        return this;
-    }
-
-    public Controller onButtonC(Command command) {
-        buttonMap.put("C", command);
-        return this;
-    }
-
-    public Controller onButtonD(Command command) {
-        buttonMap.put("D", command);
-        return this;
-    }
-
-    public Controller onButtonOne(Command command) {
-        buttonMap.put("1", command);
-        return this;
-    }
-
-
-    public Controller onButtonTwo(Command command) {
-        buttonMap.put("2", command);
-        return this;
-    }
-
-    public Controller onButtonThree(Command command) {
-        buttonMap.put("3", command);
-        return this;
-    }
-
-    public Controller onButtonFour(Command command) {
-        buttonMap.put("4", command);
-        return this;
-    }
 }
---
+++ Edu/smart-home/src/test/ru/sbt/mipt/oop/commands/AllLightsOffCommandTest.java
@@ -0,0 +1,4 @@
+package ru.sbt.mipt.oop.commands;
+
+public class AllLightsOffCommandTest {
+}
