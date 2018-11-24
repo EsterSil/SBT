1b768268f5da196cbed39fe0f8c3ab0c02611cf1
EsterSil <lernichenko@phystech.edu>
2018-11-24 22:50:53 +0300

    tests+benchmarks

---
+++ Edu/CountersBench/counter-bench/src/test/java/sbt/edu/tests/CombiningTreeTest.java
@@ -0,0 +1,72 @@
+package sbt.edu.tests;
+
+import org.junit.jupiter.api.Test;
+import sharedcounter.combiningtree.CombiningTreeCounter;
+
+import java.util.concurrent.ExecutionException;
+
+public class CombiningTreeTest {
+
+    private CommonCounterTest test = new CommonCounterTest(1000);
+    CombiningTreeCounter counter = new CombiningTreeCounter(10);
+
+    @Test
+    void singleThreadTest() {
+        try {
+            test.consistencyTest(counter);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        counter.reset();
+    }
+
+    @Test
+    void twoThreadsTest() {
+        try {
+            test.lowContentionConsistencyTest(counter);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        counter.reset();
+    }
+
+    @Test
+    void manyThreadTest() {
+        try {
+            test.highContentionConsistencyTest(counter);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        counter.reset();
+    }
+    @Test
+    void uniqPerOneTest () {
+        try {
+            test.uniquenessPerOneTest(counter);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        counter.reset();
+    }
+
+    @Test
+    void uniqPerManyTest () {
+        try {
+            test.uniquenessPerManyTest(counter);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        counter.reset();
+    }
+
+    @Test
+    void uniqThroughManyTest () {
+        try {
+            test.uniquenessThroughManyTest(counter);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        counter.reset();
+    }
+
+}
