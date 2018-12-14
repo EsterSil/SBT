07a7f1ede6952b735724e063e7cae0fbbff6262f
EsterSil <lernichenko@phystech.edu>
2018-12-14 15:47:06 +0300

    second task

---
+++ Edu/Multithreading/src/main/java/sbt/edu/executionmanager/ContextImpl.java
@@ -0,0 +1,54 @@
+package sbt.edu.executionmanager;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ContextImpl implements Context {
+
+    private AtomicInteger completedTaskCount = new AtomicInteger();
+    private AtomicInteger failedTaskCount = new AtomicInteger();
+    private AtomicInteger interruptedTaskCount = new AtomicInteger();
+    private AtomicBoolean finished = new AtomicBoolean();
+    private AtomicBoolean interrupted = new AtomicBoolean();
+
+    @Override
+    public int getCompletedTaskCount() {
+        return completedTaskCount.get();
+    }
+
+    @Override
+    public int getFailedTaskCount() {
+        return failedTaskCount.get();
+    }
+
+    @Override
+    public int getInterruptedTaskCount() {
+        return failedTaskCount.get();
+    }
+
+    @Override
+    public void interrupt() {
+        interrupted.getAndSet(true);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished.get();
+    }
+
+
+    void incrementCompleteCounter() {
+        completedTaskCount.getAndIncrement();
+    }
+    void incrementFailedCounter() {
+        failedTaskCount.getAndIncrement();
+    }
+
+    boolean isInterrupted() {
+        return interrupted.get();
+    }
+
+    void setUpFinished(){
+        finished.set(true);
+    }
+}
--- Edu/Multithreading/src/main/java/sbt/edu/executionmanager/ExecutionManagerImpl.java
+++ Edu/Multithreading/src/main/java/sbt/edu/executionmanager/ExecutionManagerImpl.java
@@ -1,21 +1,57 @@
 package sbt.edu.executionmanager;
 
-import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ExecutionManagerImpl implements ExecutionManager {
+
+
     @Override
     public Context execute(Runnable callback, Runnable... tasks) {
+        ContextImpl context = new ContextImpl();
         Thread thread = new Thread(() -> {
             ExecutorService executor = Executors.newFixedThreadPool(10);
-            for (Runnable r: tasks) {
-                executor.execute(r);
+            ArrayBlockingQueue<Future> futures = new ArrayBlockingQueue<>(10);
+            Thread watcher = new Thread(() -> {
+                OUTER: while (!context.isInterrupted() && !context.isFinished()) {
+                    for (Future f : futures) {
+                        if (f.isDone()) {
+                            try {
+                                f.get();
+                                context.incrementCompleteCounter();
+                            } catch (InterruptedException e) {
+                                break OUTER;
+                            } catch (ExecutionException e) {
+                                e.printStackTrace();
+                                context.incrementFailedCounter();
+                            } finally {
+                                futures.remove(f);
+                            }
+                        }
+                    }
+                }
+                if (context.isInterrupted()){
+                    for (Future f : futures) {
+                        f.cancel(true);
+                    }
+                }
+            });
+            watcher.start();
+            for (Runnable r : tasks) {
+                futures.add(executor.submit(r));
             }
             executor.shutdown();
-        }) ;
+            try {
+                if (!executor.awaitTermination(8000, TimeUnit.MILLISECONDS)) {
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executor.shutdownNow();
+            }
+            callback.run();
+            context.setUpFinished();
+        });
+        thread.start();
 
-
-        return null;
+        return context;
     }
 }
